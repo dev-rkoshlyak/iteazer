9e86988e9adc9acc23cc9d7664de3086e06b1ecf
WslF <WasylFranchuk@gmail.com>
2016-11-01 17:38:37 +0200

    create client, that connects to all droids and rotate them

---
+++ Ollie-competition/NodeJS-server/Test/testAll.js
@@ -0,0 +1,7 @@
+var testUnit = require("./test");
+
+testUnit.test("d8e38c77d05d");
+testUnit.test("dc712fb5b631");
+testUnit.test("f15cee63622d");
+testUnit.test("c84982ebcc74");
+testUnit.test("ee42664940f4");
